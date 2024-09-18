9b5a2184d78af051907988eed6f4f1be23b82e09
RanimeeKhoury <khouryranime@gmail.com>
2024-09-18 15:13:20 +0300

    Added Hilt tag to MainActivity

--- app/src/main/java/com/example/geofencing/MainActivity.kt
+++ app/src/main/java/com/example/geofencing/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.geofencing
 
+//import LogsScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import com.example.geofencing.ui.theme.GeofencingTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             GeofencingTheme {
-
+                Surface(
+                    modifier = androidx.compose.ui.Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    LogsScreen()
+                }
             }
         }
     }
