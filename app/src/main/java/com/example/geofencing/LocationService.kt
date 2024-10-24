8b6c2ea31614331f8bfaa75d479dc6184ae32562
Ranime <khouryranime@gmail.com>
2024-10-24 19:08:24 +0300

    Handle new positions in Foreground Service on IO thread

--- app/src/main/java/com/example/geofencing/LocationService.kt
+++ app/src/main/java/com/example/geofencing/LocationService.kt
@@ -13,14 +13,26 @@ import android.os.IBinder
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import co.anbora.labs.spatia.geometry.Point
+import com.example.geofencing.data.model.Position
+import com.example.geofencing.repository.AppRepository
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LocationService : Service() {
+    @Inject
+    lateinit var repository: AppRepository
+
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
@@ -38,8 +50,17 @@ class LocationService : Service() {
             override fun onLocationResult(locationResult: LocationResult) {
                 super.onLocationResult(locationResult)
                 locationResult.lastLocation?.let {
-                    //// TODO: Handle location update
-                    Log.i("secondtag", "Location: ${it.latitude}, ${it.longitude}")
+                    CoroutineScope(Dispatchers.IO).launch {
+                        Log.i("mytag", "Location: ${it.latitude}, ${it.longitude}")
+                        repository.handleNewPosition(
+                            Position(
+                                point = Point(
+                                    it.latitude,
+                                    it.longitude
+                                ), timestamp = it.time
+                            )
+                        )
+                    }
                 }
             }
         }
